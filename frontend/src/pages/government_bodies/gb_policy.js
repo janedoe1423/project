77f2484d57db305e40a8ecbe50265a72a55c7e2f
janedoe1423 <221401016@rajalakshmi.edu.in>
2024-12-10 16:14:17 +0530

    Added government sidebar and profile page

---
+++ frontend/src/pages/government_bodies/gb_policy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PolicyImplementation = () => {
+    return (
+        <div>
+            <h2>Policy</h2>
+            {/* Add your policy content here */}
+        </div>
+    );
+};
+
+export default PolicyImplementation;
---
+++ frontend/src/pages/government_bodies/gb_rs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ResourceSupport = () => {
+    return (
+        <div>
+            <h2>Resource Support</h2>
+            {/* Add your resource support content here */}
+        </div>
+    );
+};
+
+export default ResourceSupport;
